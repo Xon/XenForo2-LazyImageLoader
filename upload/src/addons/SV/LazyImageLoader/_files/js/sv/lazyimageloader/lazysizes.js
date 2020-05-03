0a30adda75cb21e18b160cebe00be1bd72fd8821
Xon <tazipper1@hotmail.com>
2020-05-03 09:37:25 +0800

    Update to lazysizes 5.2.0

--- upload/src/addons/SV/LazyImageLoader/_files/js/sv/lazyimageloader/lazysizes.js
+++ upload/src/addons/SV/LazyImageLoader/_files/js/sv/lazyimageloader/lazysizes.js
@@ -1,11 +1,11 @@
 (function(window, factory) {
-	var lazySizes = factory(window, window.document);
+	var lazySizes = factory(window, window.document, Date);
 	window.lazySizes = lazySizes;
 	if(typeof module == 'object' && module.exports){
 		module.exports = lazySizes;
 	}
 }(typeof window != 'undefined' ?
-      window : {}, function l(window, document) {
+      window : {}, function l(window, document, Date) { // Pass in the windoe Date function also for SSR because the Date class can be lost
 	'use strict';
 	/*jshint eqnull:true */
 
@@ -56,15 +56,17 @@
 
 	var docElem = document.documentElement;
 
-	var Date = window.Date;
-
 	var supportPicture = window.HTMLPictureElement;
 
 	var _addEventListener = 'addEventListener';
 
 	var _getAttribute = 'getAttribute';
 
-	var addEventListener = window[_addEventListener];
+	/**
+	 * Update to bind to window because 'this' becomes null during SSR
+	 * builds.
+	 */
+	var addEventListener = window[_addEventListener].bind(window);
 
 	var setTimeout = window.setTimeout;
 
