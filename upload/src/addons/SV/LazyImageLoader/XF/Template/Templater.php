8eda21777514ff1932744df59bcdbc13151005bd
Xon <tazipper1@hotmail.com>
2020-06-07 05:24:39 +0800

    Properly inject loading=lazy attribute into <img> tag

--- upload/src/addons/SV/LazyImageLoader/XF/Template/Templater.php
+++ upload/src/addons/SV/LazyImageLoader/XF/Template/Templater.php
@@ -9,24 +9,30 @@ use XF\Mvc\Entity\Entity;
  */
 class Templater extends XFCP_Templater
 {
-    protected function injectLazyAttributes(&$attributes)
+    protected function injectImgLazyAttribute($url)
     {
-        if (is_array($attributes) && empty($attributes['loading']) && !empty(\XF::options()->svLazyLoadIcons))
+        if ($url && !empty(\XF::options()->svLazyLoadIcons))
         {
-            $attributes['loading'] = 'lazy';
+            $url = \str_replace('<img ', '<img loading="lazy" ', $url);
         }
 
-        return $attributes;
+        return $url;
     }
 
     public function fnAvatar($templater, &$escape, $user, $size, $canonical = false, $attributes = [])
     {
-        return parent::fnAvatar($templater, $escape, $user, $size, $canonical, $this->injectLazyAttributes($attributes));
+        return $this->injectImgLazyAttribute(parent::fnAvatar($templater, $escape, $user, $size, $canonical, $attributes));
     }
 
     public function fnThreadmarkIndexIcon(\SV\Threadmarks\XF\Template\Templater $templater, &$escape, Entity $content, \SV\Threadmarks\Entity\ThreadmarkIndex $threadmarkIndex, $size = 'l', array $attributes = [])
     {
         /** @noinspection PhpUndefinedMethodInspection */
-        return parent::fnThreadmarkIndexIcon($templater, $escape, $content, $threadmarkIndex, $size, $this->injectLazyAttributes($attributes));
+        return $this->injectImgLazyAttribute(parent::fnThreadmarkIndexIcon($templater, $escape, $content, $threadmarkIndex, $size, $attributes));
+    }
+
+    public function fnResourceIcon($templater, &$escape, \XFRM\Entity\ResourceItem $resource, $size = 'm', $href = '')
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        return $this->injectImgLazyAttribute(parent::fnResourceIcon($templater, $escape, $resource, $size, $href));
     }
 }
