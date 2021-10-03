d0e1c02cb2f1e96ee48cae4abd2dc57e3905e832
Xon <tazipper1@hotmail.com>
2021-10-03 08:42:35 +0800

    Prevent doubling images on selecting text & using quote/reply feature.

--- upload/src/addons/SV/LazyImageLoader/_files/js/sv/lazyimageloader/ls.config.js
+++ upload/src/addons/SV/LazyImageLoader/_files/js/sv/lazyimageloader/ls.config.js
@@ -5,3 +5,12 @@ window.lazySizesConfig.nativeLoading = {
         scroll: true
     },
 };
+SV = window.SV || {};
+SV.oldUnparseBbCode = XF.unparseBbCode;
+XF.unparseBbCode = function(html) {
+    var $div = $(document.createElement('div'));
+    $div.html(html);
+    $div.find('noscript').remove();
+    html = $div.html();
+    return SV.oldUnparseBbCode(html);
+};
